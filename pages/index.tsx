166c066d03f17c96541f3912cc7cce6893dbc7e6
lrypto <lloydrebello@gmail.com>
2021-06-02 13:07:02 -0400

    commented out parts that would break on first run

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import useContract from "../components/hooks/useContract";
 import useEth from "../components/hooks/useEth";
 import Button from "../components/ui/Button";
 import SimplePageBlock from "../components/ui/SimplePageBlock";
-import TMPToken from "../abis/TMPToken.sol/TMPToken.json";
+// import TMPToken from "../abis/TMPToken.sol/TMPToken.json";
 
 export default function Home() {
   const { ethersProvider, currentAddr } = useEth();
   const { createContract } = useContract(ethersProvider);
-  const TMPTOKEN_ADDR = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+  // const TMPTOKEN_ADDR = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
  
   return (
     <div className="mt-20">
@@ -20,9 +20,9 @@ export default function Home() {
             <Button
               clickAction={ async() => {
                 console.log('connecting to contract');
-                const tmpToken = await createContract(TMPTOKEN_ADDR, TMPToken.abi);
-                const balance = await tmpToken.balanceOf(currentAddr);
-                console.log('balance:', balance.toString());
+                // const tmpToken = await createContract(TMPTOKEN_ADDR, TMPToken.abi);
+                // const balance = await tmpToken.balanceOf(currentAddr);
+                // console.log('balance:', balance.toString());
               }}
             >
               connect to test contract
